b1f0c6db79093828681d1ccd4955720d3e4cc4e7
Scysta <isotericscysta@hotmail.com>
2022-07-12 12:56:30 -0400

    Updated update.sh

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ ln -sf /home/scysta/dotfiles/.zshrc /home/scysta/.zshrc
 #ln -sf /home/scysta/dotfiles/bspwm/modules /home/scysta/.config/bspwm/polybar/modules
 #ln -sf /home/scysta/dotfiles/bspwm/config.ini /home/scysta/.config/bspwm/polybar/config.ini
 #ln -sf /home/scysta/dotfiles/bspwm/sxhkdrc /home/scysta/.config/sxhkd/sxhkdrc
+#ln -sf /home/scysta/dotfiles/bspwm/bspwmrc /home/scysta/.config/bspwm/bspwmrc
 ln -sf /home/scysta/dotfiles/init.lua /home/scysta/.config/nvim
 ln -sf /home/scysta/dotfiles/lua /home/scysta/.config/nvim
 
